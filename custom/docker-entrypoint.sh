86ad50cf9b4e8b85caef2668bd598ca4b2df0375
Sam Georges <sam@daftspunk.com>
2022-05-06 09:55:29 +1000

    Demo theme imports blueprints
    Make sure they are writable

--- custom/docker-entrypoint.sh
+++ custom/docker-entrypoint.sh
@@ -8,4 +8,7 @@ echo 'Setting permissions...'
 chown -R www-data:www-data /var/www/html/storage
 chmod -R 755 /var/www/html/storage
 
+chown -R www-data:www-data /var/www/html/app
+chmod -R 755 /var/www/html/app
+
 exec "$@"
